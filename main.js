506905af5a686a35013a6e219d86a93368835516
Yorqinali <grose20030916@gmail.com>
2023-02-24 10:02:44 +0500

    if-else

---
+++ main.js
@@ -0,0 +1,409 @@
+// 1.A butun son berilgan. Jumlani rostlikka tekshiring: A son musbat
+
+// var a = 10
+// if (a > 0){
+//   console.log ('musbat');
+// } else {
+//   console.log ('manfiy');
+// }
+
+
+
+
+// 2.A butun son berilgan. Jumlani rostlikka tekshiring: A toq son
+
+// var a = 7
+// if (a % 2 != 0){
+//   console.log('toq');
+// }else{
+//   console.log('juft');
+// }
+
+
+
+
+// 3.A butun son berilgan. Junlani rostlikka tekshiring: A juft son
+
+// var a = 4
+// if (a % 2 == 0){
+//   console.log('juft');
+// }else{
+//   console.log('toq');
+// }
+
+
+
+
+// 4.Ikkita butun son berilgan. Jumlani rostlikka tekshiring. M-s a > 0 && b > 0
+
+// var a = 20
+// var b = 10
+// if(a>0 && b>0){
+//   console.log(true);
+// }else{
+//   console.log(false);
+// }
+
+
+
+
+// 5.3 ta son berilgan a,b,c. a bdan katta b c dan katta rostlikka tekshiring
+
+// var a=30
+// var b=20
+// var c=10
+// if (a>b && b>c && c<a){
+//   console.log(true);
+// }else{
+//   console.log(false);
+// }
+
+
+
+
+// 6.a, b, c, son berilgan. B son a va c sonlarni orasida yotadi.
+
+// var a=30
+// var b=20
+// var c=10
+// if (a>b && b>c){
+//   console.log(true);
+// }else{
+//   console.log(false);
+// }
+
+
+
+
+// 7.a, b sonlar berilgan sonlarni toq songa tekshiring.
+
+// var a = 7
+// var b = 9
+// if (a % 2 != 0 && b % 2 !=0){
+//   console.log('toq');
+// }else{
+//   console.log('juft');
+// }
+
+
+
+
+// 8.a,b,c,d sonlar berilgan barchasi musbat
+
+// var a=10
+// var b=20
+// var c=30
+// var d=40
+// if (a>0 && b>0 && c>0 && d>0){
+//   console.log(true);
+// }else{
+//   console.log(false);
+// }
+
+
+
+
+// 9.a,b,c sonlar berilgan kamida ikkitasi musbat berilgan
+
+// var a=-10
+// var b=20
+// var c=30
+// if (a>0 && b>0 && c<0){
+//   console.log(true);
+// }else if(a<0 && b>0 && c>0){
+//   console.log(true);
+// }else if(a>0 && b<0 && c>0){
+//   console.log(true);
+// }else if(a<0 && b<0 && c<0){
+//   console.log(false);
+// }
+
+
+
+
+// 10.hafta kunlari berilgan raqamga qarab hafta kunlarini nomini chiqaring
+
+// var  day = 1;
+// if (day==1) {
+//   console.log('dushanba');
+// } else if (day == 2){
+//   console.log('seshanba');
+// }else if (day == 3){
+//   console.log('chorshanba');
+// }else if (day == 4){
+//   console.log('payshanba');
+// }else if (day == 5){
+//   console.log('juma');
+// }else if (day == 6){
+//   console.log('shanba');
+// }else if (day == 7){
+//   console.log('yakshanba');
+// }
+
+
+
+
+// 11.Telefon nomer kodi orqali qaysi kompaniya ekanligini tekshiring
+
+// var kod=98;
+// if (kod == 90) {
+//  console.log('beline');
+// }else if (kod == 91) {
+//   console.log('beline');
+//  }else if (kod == 93) {
+//   console.log('usell');
+//  }else if (kod == 94) {
+//   console.log('usell');
+//  }else if (kod == 97) {
+//   console.log('mobiuz');
+//  }else if (kod == 98) {
+//   console.log('perfectum mobile');
+//  }else if (kod == 99) {
+//   console.log('uzmobile');
+//  }else if (kod == 33) {
+//   console.log('humans');
+//  }else if (kod == 50) {
+//   console.log('usell');
+//  }
+
+
+
+
+// 12.Butun son berilgan agar son musbat bo'lsa unda sonni bittaga oshiring aks xolda 1taga kamaytiring
+
+// var a=10
+// if(a>0) {
+//   console.log(a+=1);
+// }else{
+//   console.log(a-=1);
+// }
+
+
+
+
+// 13.Butun son berilgan agar son musbat bo'lsa sonni 3 ga oshiring manfiy bo'lsa unda 2ga kamaytiring
+
+// var a=20;
+// if(a>0){
+//   console.log(a+=3);
+// }else{
+//   console.log(a-=2);
+// }
+
+
+
+
+// 14.Ikkita butun son berilgan kattasin aniqlovchi dastur tuzing
+
+// var a=20;
+// var b=40;
+// if(a>b) {
+//   console.log(a);
+// }else{
+//   console.log(b);
+// }
+
+
+
+
+// 15.3 ta butun son berilgan eng kattasini aniqlovchi dastur tuzing
+
+// var a=80;
+// var b=70;
+// var c=60;
+// if(a>b && a>c) {
+//   console.log(a);
+// }else if(b>a && b>c) {
+//   console.log(b);
+// }else if (c>a &&c>b) {
+//   console.log(c);
+// }
+
+
+// 16.3 ta son berilgan eng kichikin aniqlovchi dastur tuzing
+
+// var a=30
+// var b=40
+// var c=50
+// if(a<b && a<c) {
+//   console.log(a);
+// }else if(b<a && b<c) {
+//   console.log(b);
+// }else if (c<a &&c<b) {
+//   console.log(c);
+// }
+
+
+
+
+// 17.Jinsga qarab log chiqivchi dastur tuzing!
+
+// var person='male'
+// if (person == 'male') {
+//   console.log('erkak');
+// }else{
+//   console.log('ayol');
+// }
+
+
+
+// 18.Bahoga qarab stependiyani aniqlovchi dastur tuzing
+
+// var ball=3
+// if(ball == 5){
+//   console.log('700.000 som');
+// }else if( ball == 4) {
+//   console.log('600.000 som');
+// }else if (ball == 3){
+//   console.log('400.000 som');
+// }else {
+//   console.log('boshqatdan topshirish');
+// }
+
+
+
+
+
+// .19.Butun son berilgan shu songa qarab toq musbat, toq manfiy, juft musbat, juft manfiy deb chiqaruvchi dastur tuzing
+
+// var a=-10
+// if(a%2 != 0 && a>0){
+//   console.log('toq musbat');
+// }else if(a%2 == 0 && a>0){
+//   console.log('juft musbat');
+// }else if(a%2 == 0 && a<0){
+//   console.log('juft manfiy');
+// }else if(a%2 != 0 && a<0){
+//   console.log('toq manfiy');
+// }
+
+
+
+
+// 20.Yilga qarab yoshin aniqlovchi dastur tuzing
+
+// var a=2012
+// var b=2023
+// if(b>a){
+//   console.log( b-a);
+// }else{
+//   console.log('ushbu yilda tug`ilganlar hali yoshga to`lmadi!');
+// }
+
+
+
+
+// 21.3 ta butun son berilgan jumlani rostlikkar tekshiring. A <= B <= C
+
+// var a=20
+// var b=30
+// var c=40
+// if(a <= b && b <= c){
+//   console.log(true);
+// }else{
+//   console.log(false);
+// }
+
+
+
+
+// 22.ikki butun son berilgan ikki sondan faqat 1tasi toq son hisoblanadi
+
+// var a=2
+// var b=3
+// if(a%2 !=0 && b%2 ==0){
+//   console.log(a);
+// }else if(a%2 ==0 && b%2 !=0) {
+//   console.log(b);
+// }else if(a%2 !=0 && b%2 !=0) {
+//   console.log(false);
+// }else if(a%2 ==0 || b%2 ==0) {
+//   console.log(false);
+// }
+
+
+
+
+// 23.a,b,c sonlar berilgan har biri musbat son
+
+// var a = 10;
+// var b = 20;
+// var c = 30;
+// if(a>0 && b>0 && c>0){
+//   console.log(true);
+// }else{
+//   console.log(false);
+// }
+
+
+
+
+// 24.a,b,c sonlardan biri musbat son
+
+// var a = -2
+// var b = -3
+// var c = 5
+// if(a > 0 && b < 0 && c < 0){
+//   console.log(a);
+// }else if(a < 0 && b > 0 && c < 0) {
+//   console.log(b);
+// }else if(a < 0 && b < 0 && c > 0) {
+//   console.log(c);
+// }else if(a > 0 && b > 0 && c > 0) {
+//   console.log(false);
+// }else if(a < 0 && b < 0 && c < 0) {
+//   console.log(false);
+// }
+
+
+
+
+
+// 25.3 xonali son berilgan har bir raqamlari har xil
+
+// var num1 = '2';
+// var num2 = '4';
+// var num3 = '8';
+// if(num1 != num2 && num2 != num3 && num1 != num3){
+//   var result = String(num1 + num2 + num3);  
+//   console.log(result);
+// }else{
+//   console.log(false);
+// }
+
+
+
+
+// 26.3 xonali son berilgan raqamlari ketma ket o'sib boruvchi joylashgan
+
+// var num1 = '1';
+// var num2 = '2';
+// var num3 = '3';
+// if(num1 < num2 && num2 < num3 && num1 < num3){
+//   var result = String(num1 + num2 + num3);  
+//   console.log(result);
+// }else{
+//   console.log(false);
+// }
+
+
+
+
+// 27.Kompyuter nomiga qarab kompyuter summasini chiqaruvchi dastur yarating
+
+// var name= 'macbook'
+// if(name == 'hp'){
+//   console.log('7.000.000 som');
+// }else if( name == 'macbook' ) {
+//   console.log('12.000.000 som');
+// }else if (name == 'asus'){
+//   console.log('6.000.000 som');
+// }else if (name == 'acer') {
+//   console.log('8.000.000 som');
+// }else {
+//   console.log('bunday kompyuter yoq');
+// }
+
+
+
+
